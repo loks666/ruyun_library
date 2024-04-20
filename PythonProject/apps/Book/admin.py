4767f7e40fa66faff9ecb389b01b30d7da277165
jax <284190056@qq.com>
2024-04-21 04:55:25 +0800

    🍻Sort out the project structure.

--- PythonProject/apps/Book/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from apps.Book.models import Book
-
-# Register your models here.
-
-admin.site.register(Book,admin.ModelAdmin)
--- PythonProject/apps/BookType/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BookclassConfig(AppConfig):
-    name = 'apps.BookType'
